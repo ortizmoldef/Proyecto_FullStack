4e38613b68968e800d7301288575e6151a2fec10
ortizmoldef <184933180+ortizmoldef@users.noreply.github.com>
2025-04-21 16:29:14 +0200

    .
    .

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const usersRoutes = require('./routes/routesUser');
 const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 console.log("DEBUG >>> MONGO_URI =", process.env.MONGO_URI);
 
 // Middleware con límite de 5MB para JSON y URL encoded
@@ -18,8 +18,9 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 
+
 app.use(cors({
-    origin: 'http://localhost:3000', // Permite solicitudes solo desde este origen
+    origin: [frontendUrl, 'http://localhost:3000'], // Permite solicitudes solo desde este origen
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Si necesitas enviar cookies o tokens de sesión
