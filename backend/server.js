c131d7aa1ddcb5b0fac6ede628e0410728b27e95
ortizmoldef <184933180+ortizmoldef@users.noreply.github.com>
2025-04-21 17:02:26 +0200

    .
    .

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const usersRoutes = require('./routes/routesUser');
 const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3000';
 console.log("DEBUG >>> MONGO_URI =", process.env.MONGO_URI);
 
 // Middleware con límite de 5MB para JSON y URL encoded
@@ -18,15 +17,25 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 
-
-app.use(cors({
-    origin: [frontendUrl, 'http://localhost:3000'], // Permite solicitudes solo desde este origen
+const allowedOrigins = [
+    'http://localhost:3000',
+     process.env.FRONTEND_URL,
+  ];
+  
+  app.use(cors({
+    origin: (origin, callback) => {
+      // Permite solicitudes sin origin (como en herramientas tipo Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Si necesitas enviar cookies o tokens de sesión
-    preflightContinue: false,
+    credentials: true
   }));
-
+  
 // Rutas
 app.use('/api', moviesRoutes);
 app.use('/api', usersRoutes);
