b02d9bf30dd22d4a02e37f2ad0a2704ff5032f12
ortizmoldef <184933180+ortizmoldef@users.noreply.github.com>
2025-04-21 19:50:14 +0200

    .
    .

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000', // Usa la variable de entorno o localhost por defecto
+  baseURL: process.env.REACT_APP_API_URL, // Usa la variable de entorno o localhost por defecto
   headers: {
     'Content-Type': 'application/json',
   },
