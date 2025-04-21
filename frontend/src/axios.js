491fe51b76b493d71284504ed938762f5118fc6a
ortizmoldef <184933180+ortizmoldef@users.noreply.github.com>
2025-04-21 22:08:06 +0200

    .
    .

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || process.env.REACT_APP_API_URL_LOCAL,
+  baseURL: process.env.NODE_ENV === 'development' 
+    ? 'http://localhost:5000' // URL local en desarrollo
+    : process.env.REACT_APP_API_URL, // URL desplegada en producción
   headers: {
     'Content-Type': 'application/json',
   },
